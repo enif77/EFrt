6fa7740f8cd6d1dafa9128563e9fcaa2381f1b4a
Přemysl Fára <enif@email.cz>
2021-03-01 20:27:55 +0100

    Store word tests.

--- EFrt_Tests/Libs.Core/StoreActionTests.cs
+++ EFrt_Tests/Libs.Core/StoreActionTests.cs
@@ -2,12 +2,11 @@
 
 namespace EFrt_Tests.Libs.Core
 {
-    using System;
-
     using Xunit;
 
     using EFrt;
     using EFrt.Core;
+    using EFrt.Core.Words;
     
     
     public class StoreActionTests
@@ -18,6 +17,20 @@ namespace EFrt_Tests.Libs.Core
             
             _interpreter = InterpreterFactory.CreateWithDefaults();
             _interpreter.AddCoreLibrary();
+            _storeWord = _interpreter.GetWord("!");
+            _fetchWord = _interpreter.GetWord("@");
+        }
+
+        [Fact]
+        public void IsStoreWordTest()
+        {
+            Assert.Equal("!", _storeWord.Name);
+        }
+        
+        [Fact]
+        public void IsFetchWordTest()
+        {
+            Assert.Equal("@", _fetchWord.Name);
         }
 
         [Fact]
@@ -25,31 +38,37 @@ namespace EFrt_Tests.Libs.Core
         {
             Assert.Equal(0, _interpreter.State.Heap.ReadInt32(4));
             
-            _interpreter.Evaluate("123 4 !");
+            // -> 123 4 !
+            _interpreter.Push(123);
+            _interpreter.Push(4);
+            _storeWord.Action();
             
             Assert.Equal(123, _interpreter.State.Heap.ReadInt32(4));
             
-            _interpreter.Evaluate("4 @");
+            // -> 4 @
+            _interpreter.Push(4);
+            _fetchWord.Action();
             
             Assert.Equal(123, _interpreter.Pop());
         }
         
-        // [Fact]
-        // public void StoreToNotCellAlignedAddressTest()
-        // {
-        //     // 3 = not cell-aligned address.
-        //     var exception = Assert.Throws<InterpreterException>(() => _interpreter.Evaluate("123 3 !"));
-        //     
-        //     // -23 address alignment exception
-        //     Assert.Equal(-9, exception.ExceptionCode);
-        // }
+        [Fact]
+        public void StoreToNotCellAlignedAddressTest()
+        {
+            // -> 123 3 !
+            // 3 = not cell-aligned address.
+            _interpreter.Push(123);
+            _interpreter.Push(3);
+            
+            var exception = Assert.Throws<InterpreterException>(()
+                => _storeWord.Action());
+            
+            // -23 address alignment exception
+            Assert.Equal(-23, exception.ExceptionCode);
+        }
 
         private readonly IInterpreter _interpreter;
+        private readonly IWord _storeWord;
+        private readonly IWord _fetchWord;
     }
 }
-
-/*
- 
- 
-  
- */
