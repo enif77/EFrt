15f06dd6c226c314d7d6aa720b1d0cd57aea1b54
Přemysl Fára <enif@email.cz>
2021-03-01 16:28:40 +0100

    Heap extensions tests.

---
+++ EFrt_Tests/InterpreterHeapExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿/* EFrt - (C) 2021 Premysl Fara  */
+
+namespace EFrt_Tests
+{
+    using System;
+
+    using Xunit;
+
+    using EFrt;
+    using EFrt.Core;
+    
+    
+    public class InterpreterHeapExtensionsTests
+    {
+        public InterpreterHeapExtensionsTests()
+        {
+            _interpreter = InterpreterFactory.CreateWithDefaults();
+        }
+
+        [Fact]
+        public void ValidCellAlignedAddressTest()
+        {
+            // 4 = valid address.
+            _interpreter.CheckCellAlignedAddress(4);
+            
+            // Heap.Items.Length - 4 = valid address.
+            _interpreter.CheckCellAlignedAddress(_interpreter.State.Heap.Items.Length - 4);
+        }
+        
+        [Fact]
+        public void InvalidCellAlignedAddressTest()
+        {
+            // -1 = invalid address.
+            var exception = Assert.Throws<InterpreterException>(() => _interpreter.CheckCellAlignedAddress(-1));
+            
+            // -9 invalid memory address
+            Assert.Equal(-9, exception.ExceptionCode);
+            
+            // Heap.Items.Length + 100 = invalid address.
+            exception = Assert.Throws<InterpreterException>(() => _interpreter.CheckCellAlignedAddress(_interpreter.State.Heap.Items.Length + 100));
+            
+            // -9 invalid memory address
+            Assert.Equal(-9, exception.ExceptionCode);
+        }
+        
+        [Fact]
+        public void CellAlignedAddressTest()
+        {
+            // 4 = cell-aligned address.
+            _interpreter.CheckCellAlignedAddress(4);
+        }
+        
+        [Fact]
+        public void CellNotAlignedAddressTest()
+        {
+            // 3 = not cell-aligned address.
+            var exception = Assert.Throws<InterpreterException>(() => _interpreter.CheckCellAlignedAddress(3));
+            
+            // -23 address alignment exception
+            Assert.Equal(-23, exception.ExceptionCode);
+        }
+       
+
+        private readonly IInterpreter _interpreter;
+    }
+}
+
+/*
+ 
+ 
+  
+ */
