1471d2e7d3265d5d58013f528004840e3e89f4ee
Přemysl Fára <enif@email.cz>
2021-03-03 20:23:14 +0100

    Comma word tests.

---
+++ EFrt_Tests/Libs.Core/CommaActionTests.cs
@@ -0,0 +1,75 @@
+﻿/* EFrt - (C) 2021 Premysl Fara  */
+
+namespace EFrt_Tests.Libs.Core
+{
+    using Xunit;
+
+    using EFrt;
+    using EFrt.Core;
+    using EFrt.Core.Words;
+    
+    
+    public class CommaActionTests
+    {
+        public CommaActionTests()
+        {
+            _interpreter = InterpreterFactory.CreateWithDefaults();
+            _interpreter.AddCoreLibrary();
+            _commaWord = _interpreter.GetWord(",");
+        }
+        
+        [Fact]
+        public void IsCommaWordTest()
+        {
+            Assert.Equal(",", _commaWord.Name);
+        }
+        
+        [Fact]
+        public void StoreToAddressTest()
+        {
+            Assert.Equal(0, _interpreter.State.Heap.ReadInt32(0));
+            
+            // -> 123 4 !
+            _interpreter.Push(123);
+            _commaWord.Action();
+            
+            Assert.Equal(123, _interpreter.State.Heap.ReadInt32(0));
+            
+            // -> 1_234_567_890 4 !
+            _interpreter.Push(1_234_567_890);
+            _commaWord.Action();
+            
+            Assert.Equal(1_234_567_890, _interpreter.State.Heap.ReadInt32(4));
+        }
+        
+        [Fact]
+        public void StoreToNotCellAlignedAddressTest()
+        {
+            // 3 ALLOC 123
+            // 3 = not cell-aligned HERE address.
+            _interpreter.State.Heap.Alloc(3);
+            _interpreter.Push(123);
+            
+            var exception = Assert.Throws<InterpreterException>(()
+                => _commaWord.Action());
+            
+            // -23 address alignment exception
+            Assert.Equal(-23, exception.ExceptionCode);
+        }
+
+        [Fact]
+        public void ExpectedParametersNotFound()
+        {
+            // The value parameter is missing.
+            var exception = Assert.Throws<InterpreterException>(()
+                => _commaWord.Action());
+            
+            // -4 stack underflow
+            Assert.Equal(-4, exception.ExceptionCode);
+        }
+        
+        
+        private readonly IInterpreter _interpreter;
+        private readonly IWord _commaWord;
+    }
+}
